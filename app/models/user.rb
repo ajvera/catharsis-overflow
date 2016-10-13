8e84ee651f2a7ccefc0a8109b1e6daab906cae1a
Joan Petersohn <jepetersohn@users.noreply.github.com>
2016-10-13 14:34:41 -0500

    Update user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ActiveRecord::Base
   has_many :questions
   has_many :answers
   has_many :comments
-  has_many :votes, :as => :voteable
+  has_many :votes
  
 
   validates :name, presence: true
