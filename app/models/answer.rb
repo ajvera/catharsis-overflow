83f45adc9a9b82caad11e6b644b3a080ec1ba731
Joan Petersohn <jepetersohn@users.noreply.github.com>
2016-10-13 14:22:47 -0500

    Update answer.rb

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -1,5 +1,5 @@
 class Answer < ActiveRecord::Base
-  belongs_to :responders, {class_name: "User", foreign_key: :user_id}
+  belongs_to :responders, {class_name: "User"}
   has_many :questions
   has_many :comments, as: :commentable
   has_many :votes, as: :votable
