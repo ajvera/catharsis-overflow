14aa57b419711c5b261feb822bbff1a32b03233c
ajvera <adjove@gmail.com>
2016-10-13 14:54:37 -0500

    add bcrypt methods to user model, build authenticate method

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,5 +9,18 @@ class User < ActiveRecord::Base
   validates :name, presence: true
   validates :email, presence: true
   validates :username, presence: true
+
+  def password
+  	@password ||= BCrypt::Password.new(self.hashed_password)
+  end
+
+  def password=(plaintext)
+  	@password = BCrypt::Password.create(plaintext)
+  	self.hashed_password = @password
+  end
+
+  def authenticate(password)
+  	self.password == password
+  end
  
 end
