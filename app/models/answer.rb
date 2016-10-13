1c1108a9c07aef56995b4076d6d94e653498c34b
Stephanie Lane <Stephmglane@gmail.com>
2016-10-13 12:03:39 -0500

    belongs_to responders

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -1,5 +1,5 @@
 class Answer < ActiveRecord::Base
-  has_many :responders, {class_name: "User", foreign_key: :user_id}
+  belongs_to :responders, {class_name: "User", foreign_key: :user_id}
   has_many :questions
   has_many :comments, as: :commentable
   has_many :votes, as: :votable
