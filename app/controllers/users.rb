c7b9808b4c951f140e1d09e5c8a64595b7226b8b
Stephanie Lane <Stephmglane@gmail.com>
2016-10-13 16:50:25 -0500

    done

--- app/controllers/users.rb
+++ app/controllers/users.rb
@@ -3,16 +3,13 @@ get '/users/new' do
 end
 
 post '/users' do
-  user = User.new(username: params[:username], email: params[:email])
+  @user = User.new(params[:users])
 
-  user.password = params[:password]
-  user.save!
-
-  if user.save
-    session[:user_id] = user.id
-    redirect '/'
+  if @user.save
+    session[:user_id] = @user.id
+    redirect '/questions'
   else
-    @errors = user.errors.full_messages
+    @errors = @user.errors.full_messages
     erb :'/users/new'
   end
 end
