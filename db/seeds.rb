5f84b7d78d00c33083d62fcc2b3ef0dfe7cb0de7
sayamkim <sayamkim@gmail.com>
2016-10-13 17:29:45 -0500

    Add seeds.

---
+++ db/seeds.rb
@@ -0,0 +1,7 @@
+30.times{User.create(name: Faker::Name.name, email: Faker::Internet.email, username: Faker::Internet.user_name, password: Faker::Lorem.characters(8))}
+
+20.times{Question.create(title: Faker::Hipster.sentence(5), body: Faker::Hipster.paragraph, asker_id: rand(30))}
+
+20.times{Answer.create(body: Faker::Hipster.paragraph, responder_id: rand(20), question_id: rand(20))}
+
+20.times{Comment.create(body: Faker::Hipster.sentence, user_id: rand(20))}
