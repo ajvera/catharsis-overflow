5bb2767abb7c409ab1438355f51e69301a718d7b
Grimmer20 <m.m.grimm1@gmail.com>
2016-10-13 16:37:41 -0500

    Create get and post routes for new user. Create user/new.erb view

--- app/controllers/users.rb
+++ app/controllers/users.rb
@@ -6,7 +6,6 @@ post '/users' do
   user = User.new(username: params[:username], email: params[:email])
 
   user.password = params[:password]
-  #user.hashed_password = BCrypt::Password.create(params[:password]) **Line 11 is correct way to hash password**
   user.save!
 
   if user.save
