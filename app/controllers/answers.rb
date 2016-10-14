0c7e55b23e6b330259884f6a89ce4054c6c847af
Joan Petersohn <email": null>
2016-10-14 11:40:30 -0500

    working on getting new answers to save

--- app/controllers/answers.rb
+++ app/controllers/answers.rb
@@ -1,4 +1,11 @@
 post 'questions/:id' do
+  @responder = current_user
+  @answer = @responder.answer.create(params[:answer])
+
+  if @answer.save
+    redirect "questions/:id"
+  end
+  #add logic to display "answer cant be blank" error
 end
 
 get 'questions/:id' do
