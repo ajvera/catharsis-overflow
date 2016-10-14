81232cee9eaebc56d52e1dea99373a71cb1b6cbb
ajvera <adjove@gmail.com>
2016-10-14 14:12:22 -0500

    add comment feature to answers

--- app/controllers/comments.rb
+++ app/controllers/comments.rb
@@ -1,4 +1,4 @@
-post '/questions/:id/comments' do
+post '/questions/:q_id/comments' do
 	if logged_in?
 		@comment = Comment.new(user_id: current_user.id, commentable_id: params[:id],commentable_type: "Question", body: params[:comment] )
 		if @comment.save
@@ -9,5 +9,14 @@ post '/questions/:id/comments' do
 	end
 end
 
-post '/questions/:id/answers/:id/comments' do
+post '/questions/:q_id/answers/:a_id/comments' do
+	p params	
+	if logged_in?
+		@comment = Comment.new(user_id: current_user.id, commentable_id: params[:a_id],commentable_type: "Answer", body: params[:comment] )
+		if @comment.save
+			redirect "/questions/#{params[:q_id]}"
+		end
+	else
+		"You must be logged in to comment!"
+	end
 end
